1e35b506786d65c3cf58f3c6fb5a6a3c3ceb79f9
H.Hiro <main@hhiro.net>
2020-06-07 20:33:01 +0900

    (1) Added a class to build Rglpk coefficient matrix from names (2) To avoid ending with "no solution" for non-connected graphs, replaced constraint of "at least one edge is used between non-connected components" with "subtract large value if no edge between non-connected component is used"

--- rglpk_shiritori.rb
+++ rglpk_shiritori.rb
@@ -125,29 +125,145 @@ module ShiritoriStatus
     attr_reader :links, :nodes, :words
   end
 
+  class ProblemBuilder
+    def ProblemBuilder.parse_range(range)
+      if !range[0]
+        if !range[1]
+          raise ArgumentError, "Both bounds of a variable or a constraint must not be nil"
+        else
+          # LowerBound = -infinity
+          [Rglpk::GLP_UP, 0, range[1]]
+        end
+      else
+        if !range[1]
+          # UpperBound = +infinity
+          [Rglpk::GLP_LO, range[0], 0]
+        else
+          if range[0] == range[1]
+            [Rglpk::GLP_FX, range[0], range[1]]
+          else
+            [Rglpk::GLP_DB, range[0], range[1]]
+          end
+        end
+      end
+    end
+
+    # rglpkをそのまま使うには、何列目が何の変数なのか自分で把握しておかないとならないが、
+    # これを任意の名前で管理できるようにする
+    def initialize(options)
+      @options = options
+      @constraints = {} # @constraints[constraint_name] = [lower_bound_of_value, upper_bound_of_value]
+      @variables = {} # @variables[variable_name] = [lower_bound_of_value, upper_bound_of_value]
+      @coefficients = Hash.new{ |h, k| h[k] = {} } # @coefficients[constraint_name][variable_name] = coefficient_of_constraint
+      @objective = {} # @objective[variable_name] = coefficient_of_objective
+    end
+    attr_reader :options, :constraints, :variables, :coefficients, :objective
+
+    def build
+      # Rglpk::Problem のオプションを設定
+      @problem = Rglpk::Problem.new
+      @options.each_pair do |key, val|
+        if key !~ /\A[_A-Za-z][_0-9A-Za-z]*(\.[_A-Za-z][_0-9A-Za-z]*)*\z/
+          raise ArgumentError, "Invalid option name: #{key}"
+        end
+
+        key_list = key.split(".")
+        pos = @problem
+        key_list.each_with_index do |key, i|
+          if i == key_list.size - 1
+            pos.__send__("#{key}=", val)
+          else
+            pos = pos.__send__(key)
+          end
+        end
+      end
+
+      constraint_names = @constraints.keys
+      variable_names = @variables.keys
+
+      # 変数を用意
+      cols = @problem.add_cols(variable_names.size)
+      variable_names.each_with_index do |var, j|
+        cols[j].name = var
+        bounds = ProblemBuilder.parse_range(@variables[var])
+        cols[j].set_bounds(*bounds)
+        if bounds[1].kind_of?(Integer) and bounds[2].kind_of?(Integer)
+          cols[j].kind = Rglpk::GLP_IV
+        else
+          cols[j].kind = Rglpk::GLP_CV
+        end
+      end
+
+      # 制約を用意
+      rows = @problem.add_rows(constraint_names.size)
+      constraint_names.each_with_index do |con, i|
+        rows[i].name = con
+        rows[i].set_bounds(*ProblemBuilder.parse_range(@constraints[con]))
+      end
+
+      # 最適化したい問題の係数を設定
+      @objective.each_key do |var|
+        unless @variables.include?(var)
+          raise RuntimeError, "Objective function contains an undefined variable #{var.inspect}"
+        end
+      end
+      @problem.obj.coefs = variable_names.map{ |var| @objective.fetch(var){ 0 } }
+
+      # 係数を用意
+      @coefficients.each_pair do |con, row|
+        unless @constraints.include?(con)
+          raise RuntimeError, "Coefficients contain an undefined constraint #{con.inspect}"
+        end
+        row.each_key do |var|
+          unless @variables.include?(var)
+            raise RuntimeError, "Coefficients contain an undefined variable #{var.inspect}"
+          end
+        end
+      end
+
+      mat = []
+      constraint_names.each do |con|
+        variable_names.each do |var|
+          mat << @coefficients.fetch(con).fetch(var, 0)
+        end
+      end
+      @problem.set_matrix(mat)
+
+      @problem
+    end
+  end
+
   class OptimalEdges
     GLP_CONSTANTS = Hash[*(Rglpk.constants.map{ |c| [Rglpk.const_get(c), c] }.flatten(1))]
 
-    # 連結成分を取得する
+    # ダミー起点から辿ることのできる連結成分を取得する
     # 返り値は[辺集合, 頂点集合]。辺集合は名前、頂点集合は番号。
     # ※頂点集合は番号で見るとダミー起点とダミー終点が重複しているのが気になるが、
     #   この問題の場合、ダミー起点とダミー終点は必ずともに連結成分に含まれるので問題ない
-    def retrieve_connected_edges_and_nodes(nodes, nodes_begin, nodes_end, cols)
+    def retrieve_connected_edges_and_nodes(nodes, rglpk_problem)
       visited = Set.new
-      stack = [nodes.size]
+      stack = ["^"] # ダミー起点のみが入った状態
       result_edges = Multiset.new
+      nodes_end = nodes + ["$"]
       
       until stack.empty?
         n = stack.pop
         next if visited.include?(n)
-        visited << n # 番号で見るとダミー起点とダミー終点が重複しているのが気になるが、
-                    # ダミー終点からの移動は考慮する必要がないので問題なし
+        visited << n
         
-        nodes_end.each_index do |j|
-          if cols[n * nodes_end.size + j].mip_val > 0
-            stack << j
+        nodes_end.each do |m|
+          col = nil
+          begin
+            col = rglpk_problem.cols["edge_#{n}#{m}"]
+          rescue ArgumentError
+            # If no edge is found
+            next
+          end
+          appearances = col.mip_val
+          if appearances > 0
+            stack << m
             
-            result_edges.add(nodes_begin[n]+nodes_end[j], cols[n * nodes_end.size + j].mip_val)
+            result_edges.add("#{n}#{m}", appearances)
           end
         end
       end
@@ -164,112 +280,78 @@ module ShiritoriStatus
       nodes_end = sh_prob.nodes + ["$"]   # 終点となれる文字（現れる全文字＋ダミー終点文字）
 
       # ---------- 対応する整数計画問題のオブジェクトを生成する
-      problem = Rglpk::Problem.new
-      problem.name = "shiritori"
-      problem.obj.dir = Rglpk::GLP_MAX
+      pb = ProblemBuilder.new("name" => "shiritori", "obj.dir" => Rglpk::GLP_MAX)
       
       # 制約式の名称および、制約式が取れる値の条件を入力。
       # 初期状態では制約式の数は(sh_prob.nodes.size)+2。あとで増えていく
-      rows = problem.add_rows(sh_prob.nodes.size + 2)
-      sh_prob.nodes.each_with_index do |n, i|
-        rows[i].name = "C_#{n}" # ダミー以外の文字については入次数＝出次数
-        rows[i].set_bounds(Rglpk::GLP_FX, 0.0, 0.0)
+
+      sh_prob.nodes.each do |n|
+        # ダミー以外の文字については入次数＝出次数
+        pb.constraints["degree_#{n}"] = [0, 0]
       end
-      rows[sh_prob.nodes.size].name = "C_^" # ダミー起点文字からの出次数は1
-      rows[sh_prob.nodes.size].set_bounds(Rglpk::GLP_FX, 1.0, 1.0)
-      rows[sh_prob.nodes.size+1].name = "C_$" # ダミー終点文字への入次数は1
-      rows[sh_prob.nodes.size+1].set_bounds(Rglpk::GLP_FX, 1.0, 1.0)
+      pb.constraints["degree_^"] = [1, 1] # ダミー起点文字からの出次数は1
+      pb.constraints["degree_$"] = [-1, -1] # ダミー終点文字への入次数は1
       
-      # 変数（辺）の名称および、変数が取れる値の条件を入力。
-      # 変数の数は(nodes_begin.size * nodes_end.size)で固定。
-      # 起点がi、終点がjである辺は、cols[i * nodes_end.size + j] で表す。
-      cols = problem.add_cols(nodes_begin.size * nodes_end.size)
-      
-      i = 0
-      nodes_begin.each do |nb|
-        nodes_end.each do |ne|
-          cols[i].name = "x_#{nb},#{ne}"
-          if nb == '^' || ne == '$'
-            cols[i].set_bounds(Rglpk::GLP_DB, 0.0, 1.0)
-          else
-            if sh_prob.links["#{nb}#{ne}"].empty?
-              cols[i].set_bounds(Rglpk::GLP_FX, 0.0, 0.0)
-            else
-              cols[i].set_bounds(Rglpk::GLP_DB, 0.0, sh_prob.links["#{nb}#{ne}"].size.to_f)
-            end
-          end
-          cols[i].kind = Rglpk::GLP_IV
-          
-          i += 1
+      # 変数（辺）の名称および、変数が取れる値の条件を入力
+      # coefficientsは、出次数 - 入次数で計算
+      sh_prob.nodes.each do |n|
+        pb.variables["edge_^#{n}"] = [0, 1]
+        pb.coefficients["degree_^"]["edge_^#{n}"] = 1
+        pb.coefficients["degree_#{n}"]["edge_^#{n}"] = -1
+
+        pb.variables["edge_#{n}$"] = [0, 1]
+        pb.coefficients["degree_#{n}"]["edge_#{n}$"] = 1
+        pb.coefficients["degree_$"]["edge_#{n}$"] = -1
+      end
+
+      sh_prob.links.each_pair do |link, words|
+        pb.variables["edge_#{link}"] = [0, words.size]
+        if link[0] != link[1]
+          pb.coefficients["degree_#{link[0]}"]["edge_#{link}"] = 1
+          pb.coefficients["degree_#{link[1]}"]["edge_#{link}"] = -1
+        else
+          pb.coefficients["degree_#{link[0]}"]["edge_#{link}"] = 0
+          pb.coefficients["degree_#{link[1]}"]["edge_#{link}"] = 0
         end
+
+        pb.objective["edge_#{link}"] = 1 # 何を最大化したいか示す係数
+        # 今回の場合は、全変数の総和なので、1.0を並べたものを与える。
       end
-      
-      # 何を最大化したいか示す係数。
-      # 今回の場合は、全変数の総和なので、1.0を並べたものを与える。
-      problem.obj.coefs = [1.0] * (nodes_begin.size * nodes_end.size)
-      
-      # 制約式と変数を結ぶ行列。
-      mat = []
-      # (1. ダミー以外の文字については、入次数＝出次数)
-      sh_prob.nodes.each_with_index do |n, q|
-        nodes_begin.each_with_index do |nb, i|
-          nodes_end.each_with_index do |ne, j|
-            if i == q && j != q
-              mat << 1
-            elsif i != q && j == q
-              mat << -1
-            else
-              mat << 0
-            end
-          end
-        end
-      end
-      # (2. ダミー起点文字からの出次数は1)
-      nodes_begin.each_with_index do |nb, i|
-        nodes_end.each_with_index do |ne, j|
-          mat << (i == sh_prob.nodes.size ? 1 : 0)
-        end
-      end
-      # (3. ダミー終点文字への入次数は1)
-      nodes_begin.each_with_index do |nb, i|
-        nodes_end.each_with_index do |ne, j|
-          mat << (j == sh_prob.nodes.size ? 1 : 0)
-        end
-      end
-      
-      problem.set_matrix(mat)
       
       # ---------- 最長経路が確定するまでループ
       k = 0
       @best = []
       while true
+        STDERR.puts "k == #{k}"
+        rglpk_problem = pb.build
+
         # まずは線形計画問題/整数計画問題を解く
-        code = problem.simplex
+        code = rglpk_problem.simplex
         if code != 0
           STDERR.puts "Unexpected Error: Simplex solver ended with the code #{GLP_CONSTANTS[code]}."
           break
         end
           
-        len = problem.obj.get
+        len = rglpk_problem.obj.get
           
-        code = problem.mip
+        code = rglpk_problem.mip
         if code != 0
           STDERR.puts "Unexpected Error: MIP solver ended with the code #{GLP_CONSTANTS[code]}."
           break
         end
           
-        len = problem.obj.mip
+        len = rglpk_problem.obj.mip - sh_prob.words.size * k
           
 STDERR.puts "Current upper bound of length: #{len}"
         # 結果として得られた経路が連結か？
-        connected_edges, connected_nodes = retrieve_connected_edges_and_nodes(sh_prob.nodes, nodes_begin, nodes_end, cols)
-          
+        connected_edges, connected_nodes = retrieve_connected_edges_and_nodes(sh_prob.nodes, rglpk_problem)
+        
         # ----- 使った辺がすべて連結なら、そこで終了
-        if len == connected_edges.size
+        if len == connected_edges.size - 2 # ダミー始点やダミー終点も含んでいるため、2つ減らさないとならない。
           @best = connected_edges if connected_edges.size > @best.size
           break
         end
-          
+        
         # ----- 連結でない場合、
         if len < @best.size
           # もし今回「可能性あり」と判断された（連結ではないかもしれない）
@@ -280,28 +362,21 @@ STDERR.puts "Current upper bound of length: #{len}"
           
         # 今回連結と判断された辺が暫定1位か判定
         @best = connected_edges if connected_edges.size > @best.size
-STDERR.puts "Current best length: #{@best.size}"
+STDERR.puts "Current best length (picking up only connected): #{@best.size - 2}"
         
-        # 制約「今回連結だった頂点の集合と、それ以外の集合の間で、少なくとも1つの辺がある」
-        # を追加する
-        row = problem.add_row
-        row.name = "C_{k=#{k}"
-        row.set_bounds(Rglpk::GLP_LO, 1.0, 0.0)
-        
-        nodes_begin.each_with_index do |nb, i|
-          nodes_end.each_with_index do |ne, j|
-            if i != nodes.size && j != nodes.size && xor(connected_nodes.include?(i), connected_nodes.include?(j))
-              mat << 1
-            else
-              mat << 0
-            end
+        # 変数「今回連結だった頂点の集合と、それ以外の集合の間を結ぶような辺を使ったこと」を追加する
+        # これが一つも利用されなかった場合、スコアが強制的に引き下げられる（全単語数分だけ引かれる）
+        pb.variables["connected_#{k}"] = [0, 1]
+        pb.constraints["set_connected_#{k}"] = [0, nil]
+        sh_prob.links.each_key do |link|
+          if xor(connected_nodes.include?(link[0]), connected_nodes.include?(link[1]))
+            pb.coefficients["set_connected_#{k}"]["edge_#{link}"] = 1
           end
         end
-        
-        problem.set_matrix(mat)
-        
+        pb.coefficients["set_connected_#{k}"]["connected_#{k}"] = -1
+        pb.objective["connected_#{k}"] = sh_prob.words.size
+
         k += 1
-STDERR.puts "k == #{k}"
       end
     end
     attr_reader :best
